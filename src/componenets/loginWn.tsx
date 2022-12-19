09dc3c17c9fe207dcba964719f64f329798cc5ad
pooriya mosavi <super.pra2017@gmail.com>
2022-12-19 16:27:33 +0330

    Update loginWn.tsx

--- src/componenets/loginWn.tsx
+++ src/componenets/loginWn.tsx
@@ -1,15 +1,23 @@
-import React from 'react'
-import { useAppDispatch, useAppSelector } from '../redux/app/hooks'
-import { setLoginWn } from '../redux/app/features/inputs/inputSlice'
+import React from "react";
+import { useAppDispatch, useAppSelector } from "../redux/app/hooks";
+import { setLoginWn } from "../redux/app/features/inputs/inputSlice";
 
-type Props = {}
+type Props = {};
 
 export default function LoginWn({}: Props) {
-    const dispatch = useAppDispatch()
-    const loginWnStatus = useAppSelector(state=>state.inp.loginWn)
+  const dispatch = useAppDispatch();
+  const loginWnStatus = useAppSelector((state) => state.inp.loginWn);
   return (
-    <div className={`fixed bg-black h-screen w-full transition-opacity duration-300 top-0 left-0 ${loginWnStatus?"z-50 bg-opacity-50":"-z-50 bg-opacity-0"} `} onClick={()=>dispatch(setLoginWn(false))}>
-        aefdw
+    <div
+      className={`fixed bg-black h-screen w-full transition-opacity duration-300 top-0 left-0 ${
+        loginWnStatus ? " bg-opacity-50" : " bg-opacity-0"
+      } z-50 `}
+      onClick={(e) => {
+        dispatch(setLoginWn(false));
+        e.stopPropagation();
+      }}
+    >
+      aefdw
     </div>
-  )
+  );
 }
