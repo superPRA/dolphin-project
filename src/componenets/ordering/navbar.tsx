34aa0861d6c9b6cb441d7d35d7aeaceecfb94128
pooriya mosavi <super.pra2017@gmail.com>
2022-12-22 14:30:03 +0330

    Update navbar.tsx

--- src/componenets/ordering/navbar.tsx
+++ src/componenets/ordering/navbar.tsx
@@ -7,7 +7,7 @@ export default function Navbar({}: Props) {
   return (
     <div className='flex justify-start items-center mt-10 relative top-[1px] z-[2]'>
         <NavLink to='/order' end  className='px-4 py-2 text-lg hover:text-red-600 transition-all w-full bk2:w-auto text-center'>منو غذا</NavLink>
-        <NavLink to="order/info" className='px-4 py-2 text-lg hover:text-red-600 transition-all w-full bk2:w-auto text-center'>اطلاعات رستوران</NavLink>
+        <NavLink to="/order/info" className='px-4 py-2 text-lg hover:text-red-600 transition-all w-full bk2:w-auto text-center'>اطلاعات رستوران</NavLink>
     </div>
   )
 }
